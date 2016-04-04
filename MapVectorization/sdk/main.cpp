9f074f6da5000dabf106a0beff08dca67be1c63f
v1cb <victor.borisov1@gmail.com>
2016-04-04 18:19:43 +0300

    3rdparty: OpenCV added

---
+++ MapVectorization/sdk/main.cpp
@@ -0,0 +1,18 @@
+﻿// sdk.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "opencv2/opencv.hpp" 
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+  cv::Mat img = cv::imread("fon.bmp"); //Считываем картинку в матрицу img
+  cv::waitKey();
+
+	return 0;
+}
+
