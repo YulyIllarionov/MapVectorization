d1792a38e6fd7a6c58f34ad1c4aa47d1ee50fa2b
zkoalexey <zkoalexey@gmail.com>
2016-04-07 00:16:36 +0300

    littleimprov случайно папку залил и некоторые улучшения

--- MapVectorization/gui/mainwindow.cpp
+++ MapVectorization/gui/mainwindow.cpp
@@ -26,7 +26,15 @@ void MainWindow::on_OpenImage_triggered()
     {
         ImageViewerWidget *temp=new ImageViewerWidget(str);
         tabs->addTab(temp,str.section('/', -1, -1));
+        QObject::connect(ui->actionZoom_in,SIGNAL(triggered(bool)),temp,SLOT(zoomIn()));
+        QObject::connect(ui->actionZoom_out,SIGNAL(triggered(bool)),temp,SLOT(zoomOut()));
         temp->setNormalSizeOfImage();
 
     }
+}
+
+void MainWindow::on_actionSharpen_triggered()
+{
+    bool ok;
+    double k=QInputDialog::getDouble(0,"Увеличение резкости","k - от 0 до 10:",5,0.0,10.0,2,&ok);
 }
