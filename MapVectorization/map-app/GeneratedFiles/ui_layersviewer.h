e3eadf51b53f4ab1b0736a62e6632f6104416334
Anton <anton.kiselev.94@inbox.ru>
2016-04-17 21:56:28 +0300

    last

---
+++ MapVectorization/map-app/GeneratedFiles/ui_layersviewer.h
@@ -0,0 +1,88 @@
+/********************************************************************************
+** Form generated from reading UI file 'layersviewer.ui'
+**
+** Created by: Qt User Interface Compiler version 5.5.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_LAYERSVIEWER_H
+#define UI_LAYERSVIEWER_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QListWidget>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_LayersViewer
+{
+public:
+    QVBoxLayout *verticalLayout;
+    QListWidget *listWidget;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *Add;
+    QPushButton *Remove;
+
+    void setupUi(QWidget *LayersViewer)
+    {
+        if (LayersViewer->objectName().isEmpty())
+            LayersViewer->setObjectName(QStringLiteral("LayersViewer"));
+        LayersViewer->resize(198, 165);
+        verticalLayout = new QVBoxLayout(LayersViewer);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        listWidget = new QListWidget(LayersViewer);
+        listWidget->setObjectName(QStringLiteral("listWidget"));
+
+        verticalLayout->addWidget(listWidget);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        Add = new QPushButton(LayersViewer);
+        Add->setObjectName(QStringLiteral("Add"));
+        QIcon icon;
+        icon.addFile(QStringLiteral(":/new/icons/add.ico"), QSize(), QIcon::Normal, QIcon::Off);
+        Add->setIcon(icon);
+
+        horizontalLayout->addWidget(Add);
+
+        Remove = new QPushButton(LayersViewer);
+        Remove->setObjectName(QStringLiteral("Remove"));
+        QIcon icon1;
+        icon1.addFile(QStringLiteral(":/new/icons/remove.ico"), QSize(), QIcon::Normal, QIcon::Off);
+        Remove->setIcon(icon1);
+
+        horizontalLayout->addWidget(Remove);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+
+        retranslateUi(LayersViewer);
+
+        QMetaObject::connectSlotsByName(LayersViewer);
+    } // setupUi
+
+    void retranslateUi(QWidget *LayersViewer)
+    {
+        LayersViewer->setWindowTitle(QApplication::translate("LayersViewer", "Form", 0));
+        Add->setText(QString());
+        Remove->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class LayersViewer: public Ui_LayersViewer {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_LAYERSVIEWER_H
