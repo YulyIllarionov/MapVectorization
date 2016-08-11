e465812927644b71d71f1fc0db7060b32584e6bd
Dmitriev <evseev.dmsr@gmail.com>
2016-08-11 16:40:25 +0300

    Доделал recognition в проекте algorithms

--- MapVectorization/algorithms/algorithms.cpp
+++ MapVectorization/algorithms/algorithms.cpp
@@ -7,7 +7,6 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 
-<<<<<<< HEAD
 #include <iostream>
 
 using namespace std;
@@ -20,20 +19,8 @@ size_t min(size_t x, size_t y, size_t z);
 bool   isRepetitive(const string& s);
 bool   sort_by_lenght(const string &a, const string &b);
 //Draw ER's in an image via floodFill
-void   er_draw(vector<Mat> &channels, vector<vector<ERStat> > &regions, vector<Vec2i> group, Mat& segmentation);
-=======
-#include "opencv2/opencv.hpp" 
-#include "opencv/cv.h"
-#include "opencv/highgui.h"
-#include <cmath>
-using namespace cv;
-//---------------------------Не для проекта-----------------------
-void saveImage(const std::string &filename, cv::Mat *img) {
-	std::string output("C:/projects/MapVectorization/MapVectorization/sample/output/");
-	output += filename;
-	cv::imwrite(output, *img);
-}
->>>>>>> a6b462f23a97ba8af749d0e157ff7318e186ad77
+void  er_draw(Mat &src, Mat &dst, ERStat& er);
+void  er_draw(Mat &src, Mat &dst, Rect region);
 
 //Perform text detection and recognition and evaluate results using edit distance
 int main(int argc, char* argv[])
@@ -80,38 +67,37 @@ int main(int argc, char* argv[])
 	std::cout << "Size of regions " << regions.size() << "\n";
     cout << "TIME_REGION_DETECTION = " << ((double)getTickCount() - t_d)*1000/getTickFrequency() << endl;
 
-    /*Mat out_img_decomposition= Mat::zeros(image.rows+2, image.cols+2, CV_8UC1);
-    vector<Vec2i> tmp_group;
+    Mat out_img_decomposition= Mat::zeros(image.rows+2, image.cols+2, CV_8UC1);
     for (int i=0; i<(int)regions.size(); i++)
     {
-        for (int j=0; j<(int)regions[i].size();j++)
-        {
-            tmp_group.push_back(Vec2i(i,j));
-        }
-        Mat tmp= Mat::zeros(image.rows+2, image.cols+2, CV_8UC1);
-        er_draw(channels, regions, tmp_group, tmp);
-        if (i > 0)
-            tmp = tmp / 2;
-        out_img_decomposition = out_img_decomposition | tmp;
-        tmp_group.clear();
-    }*/
+		er_draw(channels, out_img_decomposition, regions[i]);
+    }
 
     double t_g = (double)getTickCount();
     // Detect character groups
-    vector< vector<Vec2i> > nm_region_groups;
+    //vector< vector<Vec2i> > nm_region_groups;
     vector<Rect> nm_boxes;
     erGrouping(image, channels, points, nm_boxes, ERGROUPING_ORIENTATION_HORIZ);//, "trained_classifier_erGrouping.xml");
 	std::cout << "Size of boxes " << nm_boxes.size() << "\n";
     cout << "TIME_GROUPING = " << ((double)getTickCount() - t_g)*1000/getTickFrequency() << endl;
 
-	Mat tmp;
+	Mat out_img_decomposition_step_2 = Mat::zeros(image.rows+2, image.cols+2, CV_8UC1);
+	out_img_decomposition.copyTo(out_img_decomposition_step_2);
+	for (int i = 0; i < nm_boxes.size(); i++) 
+	{
+		Mat tmp = Mat::zeros(nm_boxes[i].height, nm_boxes[i].width, CV_8UC1);
+		tmp.copyTo(out_img_decomposition_step_2(nm_boxes[i]));
+	}
+	
+	/*Mat tmp;
 	image.copyTo(tmp);
 	for (int i = 0; i < nm_boxes.size(); i++) {
 		rectangle(tmp, nm_boxes[i], cvScalar(255, 255, 0), 4);
-	}
+	}*/
 	namedWindow("detection",WINDOW_NORMAL);
-    imshow("detection", tmp);
+    imshow("detection", out_img_decomposition_step_2);
 
+	//====================================================================================
     /*Text Recognition (OCR)*/
 
     double t_r = (double)getTickCount();
@@ -127,7 +113,7 @@ int main(int argc, char* argv[])
     float scale_img  = 600.f/image.rows;
     float scale_font = (float)(2-scale_img)/1.4f;
     vector<string> words_detection;
-
+	cout << "points size" << points.size() << endl;
     t_r = (double)getTickCount();
 
     for (int i=0; i<(int)nm_boxes.size(); i++)
@@ -136,10 +122,13 @@ int main(int argc, char* argv[])
         rectangle(out_img_detection, nm_boxes[i], Scalar(0,255,255), 3);
 
         Mat group_img = Mat::zeros(image.rows+2, image.cols+2, CV_8UC1);
-        //er_draw(channels, regions, nm_region_groups[i], group_img);
+		er_draw(out_img_decomposition, group_img, nm_boxes[i]);
+
         Mat group_segmentation;
+		//channels.copyTo(group_img);
         group_img.copyTo(group_segmentation);
-        //image(nm_boxes[i]).copyTo(group_img);
+
+		//image(nm_boxes[i]).copyTo(group_img);
         group_img(nm_boxes[i]).copyTo(group_img);
         copyMakeBorder(group_img,group_img,15,15,15,15,BORDER_CONSTANT,Scalar(0));
 
@@ -150,7 +139,7 @@ int main(int argc, char* argv[])
 
         output.erase(remove(output.begin(), output.end(), '\n'), output.end());
         //cout << "OCR output = \"" << output << "\" lenght = " << output.size() << endl;
-        if (output.size() < 3)
+        if (output.size() < 1)
             continue;
 
         for (int j=0; j<(int)boxes.size(); j++)
@@ -232,22 +221,31 @@ bool isRepetitive(const string& s)
 }
 
 
-void er_draw(vector<Mat> &channels, vector<vector<ERStat> > &regions, vector<Vec2i> group, Mat& segmentation)
+void  er_draw(Mat &src, Mat &dst, ERStat& er)
 {
-    for (int r=0; r<(int)group.size(); r++)
+
+    if (er.parent != NULL) // deprecate the root region 
     {
-        ERStat er = regions[group[r][0]][group[r][1]];
-        if (er.parent != NULL) // deprecate the root region
-        {
-            int newMaskVal = 255;
-            int flags = 4 + (newMaskVal << 8) + FLOODFILL_FIXED_RANGE + FLOODFILL_MASK_ONLY;
-            floodFill(channels[group[r][0]],segmentation,Point(er.pixel%channels[group[r][0]].cols,er.pixel/channels[group[r][0]].cols),
-                      Scalar(255),0,Scalar(er.level),Scalar(0),flags);
-        }
+        int newMaskVal = 255;
+        int flags = 4 + (newMaskVal << 8) + FLOODFILL_FIXED_RANGE + FLOODFILL_MASK_ONLY;
+        floodFill(src,dst,Point(er.pixel%src.cols,er.pixel/src.cols),Scalar(255),0,Scalar(er.level),Scalar(0),flags);
     }
+
+}
+
+void  er_draw(Mat &src, Mat &dst, Rect region)
+{
+	src(region).copyTo(dst(region));
 }
 
 bool   sort_by_lenght(const string &a, const string &b){return (a.size()>b.size());}
+
+//---------------------------Не для проекта-----------------------
+void saveImage(const std::string &filename, cv::Mat *img) {
+	std::string output("C:/projects/MapVectorization/MapVectorization/sample/output/");
+	output += filename;
+	cv::imwrite(output, *img);
+}
 ////---------------------------Не для проекта-----------------------
 //void saveImage(const std::string &filename, cv::Mat *img) {
 //	std::string output("C:/projects/MapVectorization/MapVectorization/sample/output/");
@@ -343,7 +341,7 @@ bool   sort_by_lenght(const string &a, const string &b){return (a.size()>b.size(
 //	  
 //	  return rotated;
 //}
-<<<<<<< HEAD
+
 
 //int _tmain(int argc, _TCHAR* argv[])
 //{
@@ -351,183 +349,181 @@ bool   sort_by_lenght(const string &a, const string &b){return (a.size()>b.size(
 //
 //	cv::Mat img = cv::imread(imgPath, CV_LOAD_IMAGE_GRAYSCALE);
 
-
-=======
-cv::Mat rotateImage(std::vector<cv::Point> vec_points,cv::Mat img, float angle) 
-{
-	int x1 = vec_points[0].x;
-	int y1 = vec_points[0].y;
-	int width = vec_points[1].x-vec_points[0].x;
-	int height = vec_points[2].y - vec_points[0].y;
-
-	//cv::Mat image = cv::Mat(img, cv::Rect(x1,y1, width, height)); 
-	cv::Mat image = img;
-	cv::Mat thr,dst;
-	threshold(image,thr,200,255,cv::THRESH_BINARY_INV);
-	imshow("thr",thr);
-
-	std::vector<cv::Point> points;
-	cv::Mat_<uchar>::iterator it = thr.begin<uchar>();
-	cv::Mat_<uchar>::iterator end = thr.end<uchar>();
-	for (; it != end; ++it)
-	{
-		if (*it)
-		{
-			points.push_back(it.pos());
-		}
-	}
-	cv::RotatedRect box = cv::minAreaRect(cv::Mat(points));
-	cv::Size box_size = box.size;
-	if (box.angle < -45.)
-	{
-		std::swap(box_size.width, box_size.height);
-	}
-	cv::Mat rot_mat = cv::getRotationMatrix2D(box.center,angle, 1);
-	  
-	cv::Mat rotated;
-	cv::warpAffine(image, rotated, rot_mat, image.size(), cv::INTER_CUBIC);
-	  
-	cv::Mat cropped;
-	cv::getRectSubPix(rotated, box_size, box.center, cropped);
-
-	imshow("rotated",cropped);
-	cv::waitKey(0);
-	  
-	return cropped;
-}
-
-float countAngle(String image_name) 
-{
-	Mat text = imread(image_name, CV_LOAD_IMAGE_COLOR);
-	Mat textGray;
-    Mat current;
-    cvtColor(text, textGray, CV_BGR2GRAY);
-    //GaussianBlur(gray, gray, Size(9, 9), 2, 2);
-    //blur(gray, gray, Size(3, 3));
-    int threshold = 40;
-    int minLineLength = 40;
-    int maxLineGap = 10;
-
-    cvCreateTrackbar("threshold: ", "1", &threshold, 500);
-    cvCreateTrackbar("minLineLength: ", "1", &minLineLength, 200);
-    cvCreateTrackbar("maxLineGap: ", "1", &maxLineGap, 100);
-
-	current = text.clone();
-    std::vector<Vec4i> lines;
-    //Ищем линии
-    HoughLinesP(textGray, lines, 1, CV_PI / 180, 40, 0, 50);
-	float angle=0;
-    
-	for (size_t i = 0; i < lines.size(); i++)
-    {
-    	Vec4i l = lines[i];
-		Point p1, p2;
-		p1=Point(l[0], l[1]);
-		p2=Point(l[2], l[3]);
-		angle += atan2(p1.y - p2.y, p1.x - p2.x);
-    }
-	angle = angle/lines.size();
-	angle = (angle*180.)/CV_PI;
-	if (angle < 0.)
-	{
-		angle += 180.;
-	} 
-	else if (angle > 0.) 
-	{
-		angle-=180;
-	}
-	std::cout<<angle<<std::endl;
-	return angle; 
-}
-int _tmain(int argc, _TCHAR* argv[])
-{
-	std::vector<cv::Point> data;
-	data.push_back(cv::Point(0,0));
-	data.push_back(cv::Point(200,0));
-	data.push_back(cv::Point(0,179));
-	data.push_back(cv::Point(200,179));
-	String img_name = "E:/Downloads/text22.png";
-	cv::Mat src=cv::imread(img_name,0);
-	rotateImage (data,src, countAngle(img_name));
-	
-    return 0;
-	//std::string imgPath("C:/projects/MapVectorization/MapVectorization/sample/map/black/cu71Black.png");
-
-//	cv::Mat img = cv::imread(imgPath, CV_LOAD_IMAGE_GRAYSCALE);
->>>>>>> a6b462f23a97ba8af749d0e157ff7318e186ad77
-	//cv::threshold(img, img, 0, 255, CV_THRESH_BINARY);
-	
-
-	//**
-	//Поиск текста
-	//**
-	//cvCvtColor(&img, &img, CV_RGB2BGRA);
-	////saveImage("output1.jpg", &img);
-
-	//std::vector<cv::Rect> letterBBoxes = detectLetters(img, 0);
-
-	////cv::Mat img1 = makeElementOfPyramid(img, 2);
-	////std::vector<cv::Rect> letterBBoxes1 = detectLetters(img1, 1);
-
-	////cv::Mat img2 = makeElementOfPyramid(img, 3);
-	////std::vector<cv::Rect> letterBBoxes2 = detectLetters(img2, 2);
-
-	//for (int i = 0; i< letterBBoxes.size(); i++)
-	//	cv::rectangle(img, letterBBoxes.at(i), cv::Scalar(0, 255, 0), 3, 8, 0);
-
-	////for (int i = 0; i< letterBBoxes1.size(); i++)
-	////	cv::rectangle(img, letterBBoxes1.at(i), cv::Scalar(0, 0, 255), 3, 8, 0);
-
-	////for (int i = 0; i< letterBBoxes2.size(); i++)
-	////	cv::rectangle(img, letterBBoxes2.at(i), cv::Scalar(255, 0, 0), 3, 8, 0);
-	//
-	//saveImage("output1.jpg", &img);
-
-
-
-	//**
-	//Поиск линий
-	//**
-	//RegionClassifierImpl classifier(img);
-	//output_data objects = classifier.GetOutputData();
-	////saveImage("output1.jpg", &img);
-    //
-	//cv::Mat img1(img.size(), CV_8UC3);
-	//img1.setTo(cv::Scalar(0, 0, 0));
-    //
-	//uchar step = 0;
-	////cv::Vec3b color;
-	////color[0] = 90 + 10 * (++step); color[1] = 180 + 10 * step; color[2] = 10 * step; // "Рандомный" цвет
-	////img1.at <cv::Vec3b>(37, 466) = color;
-	//
-	//cv::Vec3b color(255, 255, 255);
-	//	
-	//for (auto i : objects.regions)
-	//{	
-	//	for (auto j : i)
-	//	{
-	//		img1.at <cv::Vec3b>(j) = color;
-	//	}
-	//}
-    //
-	//cv::Vec3b color_tr(255, 0, 0); // Рисуем мусор
-	//for (auto i : objects.trash)
-	//{
-	//	for (auto j : i)
-	//	{
-	//		img1.at <cv::Vec3b>(j) = color_tr;
-	//	}
-	//}
-	// 
-	//cv::Vec3b color_ln(0, 255, 0); // Линии
-	//for (auto i : objects.lines)
-	//{
-	//	for (auto j : i)
-	//	{
-	//		img1.at <cv::Vec3b>(j) = color_ln;
-	//	}
-	//}
-    //
-	//saveImage("count3.jpg", &img1);
-	//return 0;
+//cv::Mat rotateImage(std::vector<cv::Point> vec_points,cv::Mat img, float angle) 
+//{
+//	int x1 = vec_points[0].x;
+//	int y1 = vec_points[0].y;
+//	int width = vec_points[1].x-vec_points[0].x;
+//	int height = vec_points[2].y - vec_points[0].y;
+//
+//	//cv::Mat image = cv::Mat(img, cv::Rect(x1,y1, width, height)); 
+//	cv::Mat image = img;
+//	cv::Mat thr,dst;
+//	threshold(image,thr,200,255,cv::THRESH_BINARY_INV);
+//	imshow("thr",thr);
+//
+//	std::vector<cv::Point> points;
+//	cv::Mat_<uchar>::iterator it = thr.begin<uchar>();
+//	cv::Mat_<uchar>::iterator end = thr.end<uchar>();
+//	for (; it != end; ++it)
+//	{
+//		if (*it)
+//		{
+//			points.push_back(it.pos());
+//		}
+//	}
+//	cv::RotatedRect box = cv::minAreaRect(cv::Mat(points));
+//	cv::Size box_size = box.size;
+//	if (box.angle < -45.)
+//	{
+//		std::swap(box_size.width, box_size.height);
+//	}
+//	cv::Mat rot_mat = cv::getRotationMatrix2D(box.center,angle, 1);
+//	  
+//	cv::Mat rotated;
+//	cv::warpAffine(image, rotated, rot_mat, image.size(), cv::INTER_CUBIC);
+//	  
+//	cv::Mat cropped;
+//	cv::getRectSubPix(rotated, box_size, box.center, cropped);
+//
+//	imshow("rotated",cropped);
+//	cv::waitKey(0);
+//	  
+//	return cropped;
 //}
+//
+//float countAngle(String image_name) 
+//{
+//	Mat text = imread(image_name, CV_LOAD_IMAGE_COLOR);
+//	Mat textGray;
+//    Mat current;
+//    cvtColor(text, textGray, CV_BGR2GRAY);
+//    //GaussianBlur(gray, gray, Size(9, 9), 2, 2);
+//    //blur(gray, gray, Size(3, 3));
+//    int threshold = 40;
+//    int minLineLength = 40;
+//    int maxLineGap = 10;
+//
+//    cvCreateTrackbar("threshold: ", "1", &threshold, 500);
+//    cvCreateTrackbar("minLineLength: ", "1", &minLineLength, 200);
+//    cvCreateTrackbar("maxLineGap: ", "1", &maxLineGap, 100);
+//
+//	current = text.clone();
+//    std::vector<Vec4i> lines;
+//    //Ищем линии
+//    HoughLinesP(textGray, lines, 1, CV_PI / 180, 40, 0, 50);
+//	float angle=0;
+//    
+//	for (size_t i = 0; i < lines.size(); i++)
+//    {
+//    	Vec4i l = lines[i];
+//		Point p1, p2;
+//		p1=Point(l[0], l[1]);
+//		p2=Point(l[2], l[3]);
+//		angle += atan2(p1.y - p2.y, p1.x - p2.x);
+//    }
+//	angle = angle/lines.size();
+//	angle = (angle*180.)/CV_PI;
+//	if (angle < 0.)
+//	{
+//		angle += 180.;
+//	} 
+//	else if (angle > 0.) 
+//	{
+//		angle-=180;
+//	}
+//	std::cout<<angle<<std::endl;
+//	return angle; 
+//}
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	std::vector<cv::Point> data;
+//	data.push_back(cv::Point(0,0));
+//	data.push_back(cv::Point(200,0));
+//	data.push_back(cv::Point(0,179));
+//	data.push_back(cv::Point(200,179));
+//	String img_name = "E:/Downloads/text22.png";
+//	cv::Mat src=cv::imread(img_name,0);
+//	rotateImage (data,src, countAngle(img_name));
+//	
+//    return 0;
+//	//std::string imgPath("C:/projects/MapVectorization/MapVectorization/sample/map/black/cu71Black.png");
+//
+////	cv::Mat img = cv::imread(imgPath, CV_LOAD_IMAGE_GRAYSCALE);
+//	//cv::threshold(img, img, 0, 255, CV_THRESH_BINARY);
+//	
+//
+//	//**
+//	//Поиск текста
+//	//**
+//	//cvCvtColor(&img, &img, CV_RGB2BGRA);
+//	////saveImage("output1.jpg", &img);
+//
+//	//std::vector<cv::Rect> letterBBoxes = detectLetters(img, 0);
+//
+//	////cv::Mat img1 = makeElementOfPyramid(img, 2);
+//	////std::vector<cv::Rect> letterBBoxes1 = detectLetters(img1, 1);
+//
+//	////cv::Mat img2 = makeElementOfPyramid(img, 3);
+//	////std::vector<cv::Rect> letterBBoxes2 = detectLetters(img2, 2);
+//
+//	//for (int i = 0; i< letterBBoxes.size(); i++)
+//	//	cv::rectangle(img, letterBBoxes.at(i), cv::Scalar(0, 255, 0), 3, 8, 0);
+//
+//	////for (int i = 0; i< letterBBoxes1.size(); i++)
+//	////	cv::rectangle(img, letterBBoxes1.at(i), cv::Scalar(0, 0, 255), 3, 8, 0);
+//
+//	////for (int i = 0; i< letterBBoxes2.size(); i++)
+//	////	cv::rectangle(img, letterBBoxes2.at(i), cv::Scalar(255, 0, 0), 3, 8, 0);
+//	//
+//	//saveImage("output1.jpg", &img);
+//
+//
+//
+//	//**
+//	//Поиск линий
+//	//**
+//	//RegionClassifierImpl classifier(img);
+//	//output_data objects = classifier.GetOutputData();
+//	////saveImage("output1.jpg", &img);
+//    //
+//	//cv::Mat img1(img.size(), CV_8UC3);
+//	//img1.setTo(cv::Scalar(0, 0, 0));
+//    //
+//	//uchar step = 0;
+//	////cv::Vec3b color;
+//	////color[0] = 90 + 10 * (++step); color[1] = 180 + 10 * step; color[2] = 10 * step; // "Рандомный" цвет
+//	////img1.at <cv::Vec3b>(37, 466) = color;
+//	//
+//	//cv::Vec3b color(255, 255, 255);
+//	//	
+//	//for (auto i : objects.regions)
+//	//{	
+//	//	for (auto j : i)
+//	//	{
+//	//		img1.at <cv::Vec3b>(j) = color;
+//	//	}
+//	//}
+//    //
+//	//cv::Vec3b color_tr(255, 0, 0); // Рисуем мусор
+//	//for (auto i : objects.trash)
+//	//{
+//	//	for (auto j : i)
+//	//	{
+//	//		img1.at <cv::Vec3b>(j) = color_tr;
+//	//	}
+//	//}
+//	// 
+//	//cv::Vec3b color_ln(0, 255, 0); // Линии
+//	//for (auto i : objects.lines)
+//	//{
+//	//	for (auto j : i)
+//	//	{
+//	//		img1.at <cv::Vec3b>(j) = color_ln;
+//	//	}
+//	//}
+//    //
+//	//saveImage("count3.jpg", &img1);
+//	//return 0;
+////}
+//
